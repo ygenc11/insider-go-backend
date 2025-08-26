514ab10072a0235bcf1db142e699a9c80cd5cfb5
Yusuf Genç <ygenc1536@gmail.com>
2025-08-26 15:26:36 +0300

    init

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: 'up' or 'down'")
+	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	instance, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fSrc, err := (&file.File{}).Open("cmd/migrate/migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithInstance("file", fSrc, "sqlite3", instance)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Invalid direction. Use 'up' or 'down'.")
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module insider-go-backend
 
 go 1.25.0
+
+require github.com/golang-migrate/migrate/v4 v4.18.3
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
